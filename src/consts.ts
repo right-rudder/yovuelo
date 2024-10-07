fac10b7854d62f4e9ab17b9aeeacce32e609c363
Diego <diegoyon@outlook.com>
2024-10-06 21:44:57 -0600

    add altos skeleton

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const KEYWORDS = "fligth school";
+
+export const PHONE_NUMBER = "(405) 789-1234";
+export const AIRPORT = "Sundance Airport";
+export const ADDRESS_LINE_1 = "13000 N Sara Rd";
+export const ADDRESS_LINE_2 = "Yukon, OK 73099";
+export const FACEBOOK_URL = "https://www.facebook.com/AltoFlightAcademy/";
+export const INSTAGRAM_URL = "https://www.instagram.com/altoflightacademy/";
+export const TIKTOK_URL = "https://www.tiktok.com/@altoflightacademy";
+export const EMAIL_ADDRESS = "info@altoflight.com";
