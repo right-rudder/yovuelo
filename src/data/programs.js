7ad63ac368128ad744523acf59142e6e33edf4e8
Diego <diegoyon@outlook.com>
2024-10-13 01:24:09 -0600

    add index sections

---
+++ src/data/programs.js
@@ -0,0 +1,44 @@
+export const programs = [
+  {
+    title: "Aviation Management (Licenciatura)",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/private-pilot",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+  {
+    title: "Pro Pilot (TSU)",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/instrument-rating",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+  {
+    title: "Codet Pilot (Piloto Privado)",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/commercial-pilot",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+  {
+    title: "Sobrecargo de Aviación",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/flight-instructor",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+  {
+    title: "Oficial de Operaciones (Dispatcher)",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/multi-engine-rating",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+  {
+    title: "Técnico en Mantenimiento",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/airline-transport-pilot",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+  {
+    title: "Cursos Complementarios",
+    description: "lorem ipsum dolor sit amet consectetur adipiscing elit",
+    url: "/flight-training/airline-transport-pilot",
+    image: "/src/assets/estudiantes-de-ifly-recibiendo-medalla.webp",
+  },
+];
---
+++ src/data/stats.js
@@ -0,0 +1,18 @@
+export const stats = [
+  {
+    value: 14,
+    title: "Aviones",
+  },
+  {
+    value: 5,
+    title: "Simuladores",
+  },
+  {
+    value: 1200,
+    title: "Testimoniales",
+  },
+  {
+    value: 2800,
+    title: "Estudiantes graduados el último año",
+  },
+];
