93f443ae037b9d0e261d1aa8801d11e9bb0957bc
Diego <diegoyon@outlook.com>
2024-10-12 23:17:53 -0600

    add footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,8 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "iFly";
+export const SITE_TITLE = "iFly Universidad Aeronáutica";
 export const SITE_DESCRIPTION =
   "Universidad aeoronáutica para pilotos aviadores en México";
 export const KEYWORDS = "flight school";
-
 export const PHONE_NUMBER = "(999) 920-1318";
-export const AIRPORT = "Sundance Airport";
 export const ADDRESS_LINE_1 =
   "Edificio Plaza Península. Prolongación Paseo de Montejo x calle 47, Benito Juárez Nte.";
 export const ADDRESS_LINE_2 = "Mérida, Yucatán, México";
